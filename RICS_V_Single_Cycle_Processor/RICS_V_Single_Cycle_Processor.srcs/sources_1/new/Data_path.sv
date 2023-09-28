1fc5e20ded984c4f24a14d8dfb5bc2a69161166a
VenuriAmarasinghe <125267812+VenuriAmarasinghe@users.noreply.github.com>
2023-09-28 14:32:18 +0530

    2n d

--- RICS_V_Single_Cycle_Processor/RICS_V_Single_Cycle_Processor.srcs/sources_1/new/Data_path.sv
+++ RICS_V_Single_Cycle_Processor/RICS_V_Single_Cycle_Processor.srcs/sources_1/new/Data_path.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 09/28/2023 06:02:38 AM
+// Create Date: 09/28/2023 09:08:57 AM
 // Design Name: 
-// Module Name: ALU_decorder
+// Module Name: Data_path
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,21 +20,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU_decorder #(ALU_OP_width=4,OP_width=7) (
-    input logic [OP_width-1:0] OP_code,
-    input logic [2:0] funct3,
-    input funct7_5,
-    output logic [ALU_OP_width-1:0] ALU_OPcode
-    
+module Data_path(
+
     );
     
-    always_comb begin
-    case(ALU_op) 
-    7'b00000000: begin 
-    ALU_OPcode <= 4'b0000;
-    
-    end
-    default : ALU_OPcode <= 4'b0010;
-    endcase
-    end 
+ 
 endmodule
