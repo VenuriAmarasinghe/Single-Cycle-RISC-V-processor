4433d3c4fc8af322f3c5562a5162d94cf07dfe90
VenuriAmarasinghe <125267812+VenuriAmarasinghe@users.noreply.github.com>
2023-09-30 18:17:00 +0530

    Update ALU_Module.sv

--- RICS_V_Single_Cycle_Processor/RICS_V_Single_Cycle_Processor.srcs/sources_1/new/ALU_Module.sv
+++ RICS_V_Single_Cycle_Processor/RICS_V_Single_Cycle_Processor.srcs/sources_1/new/ALU_Module.sv
@@ -26,7 +26,9 @@ module ALU_Module(
     input [3:0] ALU_ctrl,
     output reg [31:0] ALU_out,
     output reg Zero,
-    output reg Negative,
+    output reg LT,
+    output reg GT
+    //output reg Negative,
     //output reg Carry_out,
     //output reg Overflow
     );
@@ -37,49 +39,77 @@ module ALU_Module(
                 ALU_out <= In_A + In_B ;
                 
                 Zero<=(In_A + In_B==0)? 1:0;
-                Negative<=0;//?
+                LT<=0; 
+                GT<=0;
             
             end 
-            4'b0001: begin //SUB
+            4'b0001:begin //SUB
                 ALU_out <= In_A - In_B ;
                 
                 Zero<=(In_A - In_B==0)? 1:0;
-                Negative<=(In_A< In_B)? 1:0;
+                //Negative<=(In_A< In_B)? 1:0;
+                LT<=0;
+                GT<=0;
             
             end 
+           
             4'b0010:begin //SLL
-                ALU_out <= In_A >> In_B[4:0] ;
+                ALU_out <=( In_A >> In_B[4:0]) ;
                 
-                Zero<=(In_A >> In_B[4:0]==0)? 1:0;
+                Zero<=((In_A >> In_B[4:0])==0)? 1:0;
                 Negative<=0;
+                LT<=0;
+                GT<=0;
             
-            end 
-            4'b0011: begin //SLT
-                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+//            4'b0011: begin //SLT
+//                if (In_A[31]&(~In_B[32])) begin
+//                    ALU_out<= 32'b1;
+//                    LT<=1;
+//                    GT<=0;
+//                end 
+//                else if (~In_A[31]& In_B[31])begin 
+//                    ALU_out<= 32'b0;
+//                    LT<=0;
+//                    GT<=1;
                 
-                Zero<=(In_A- In_B==0)? 1:0;
-                Negative<=In_A - In_B;
-            
-            end 
-            4'b0100: begin //SLTU
-                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+//                end
+//                else if ( In_A[30:0]<In_B[30:0]) begin
+//                    ALU_out<= 32'b1;
+//                    LT<=1;
+//                    GT<=0;
+//                end 
+//                else if ( In_A==In_B) begin
+//                    ALU_out<= 32'b0;
+//                    LT<=0;
+//                    GT<=0;
+//                 end 
+//                 else begin 
+//                    ALU_out<= 32'b0;
+//                    LT<=0;
+//                    GT<=1;
+//                 end 
                 
-                Zero<=(In_A- In_B==0)? 1:0;
-                Negative<=In_A - In_B;
+               
             
-            end 
-            4'b0101: 4'b0100: begin //SLTU
-                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+//            4'b0100: begin //SLTU
+//                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
                 
-                Zero<=(In_A- In_B==0)? 1:0;
-                Negative<=In_A - In_B;
+//                Zero<=(In_A- In_B==0)? 1:0;
+//                Negative<=In_A - In_B;
             
-            end 
-            4'b0110: ALU_out <= In_A - In_B;
-            4'b0111: ALU_out <= 0;  //SLT
-            4'b1000:
-            4'b1001:
-            default : ALU_out= In_A;
+//            end 
+//            4'b0101:  ALU_out<= In_A;
+//            4'b0100: begin //SLTU
+//                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+                
+//                Zero<=(In_A- In_B==0)? 1:0;
+//                Negative<=In_A - In_B;
+            
+//            end 
+//            4'b0110: ALU_out <= In_A - In_B;
+//            4'b0111: ALU_out <= 0;  //SLT
+//            4'b1000:ALU_out <= 0;
+//            
         endcase 
     
     end
