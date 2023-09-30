75d90a1c98d0071431d03e66fc7f3f94a4ab72d5
VenuriAmarasinghe <125267812+VenuriAmarasinghe@users.noreply.github.com>
2023-09-30 10:40:09 +0530

    alu model update1

--- RICS_V_Single_Cycle_Processor/RICS_V_Single_Cycle_Processor.srcs/sources_1/new/ALU_Module.sv
+++ RICS_V_Single_Cycle_Processor/RICS_V_Single_Cycle_Processor.srcs/sources_1/new/ALU_Module.sv
@@ -27,28 +27,60 @@ module ALU_Module(
     output reg [31:0] ALU_out,
     output reg Zero,
     output reg Negative,
-    output reg Carry_out,
-    output reg Overflow
+    //output reg Carry_out,
+    //output reg Overflow
     );
     
-    always_ff @(ALU_ctrl,In_A ,In_B ) begin 
-    case(ALU_ctrl ) 
-    4'b0000:begin
-    ALU_out <= In_A & In_B ;
-    
-    if (In_A & In_B==0)Zero<=1;
-    else Zero<=0;
-    
-    end 
-    4'b0001: ALU_out <= In_A | In_B ;
-    4'b0010 :ALU_out <= In_A + In_B ;
-    4'b0011: ALU_out<= 0;
-    4'b0100: ALU_out <= In_A & (~In_B) ;
-    4'b0101: ALU_out <= In_A | (~In_B) ;
-    4'b0110: ALU_out <= In_A - In_B;
-    4'b0111: ALU_out <= 0;  //SLT
-    default : ALU_out<=0;
-    endcase 
+    always @(ALU_ctrl,In_A ,In_B ) begin 
+        case(ALU_ctrl ) 
+            4'b0000:begin //ADD
+                ALU_out <= In_A + In_B ;
+                
+                Zero<=(In_A + In_B==0)? 1:0;
+                Negative<=0;//?
+            
+            end 
+            4'b0001: begin //SUB
+                ALU_out <= In_A - In_B ;
+                
+                Zero<=(In_A - In_B==0)? 1:0;
+                Negative<=(In_A< In_B)? 1:0;
+            
+            end 
+            4'b0010:begin //SLL
+                ALU_out <= In_A >> In_B[4:0] ;
+                
+                Zero<=(In_A >> In_B[4:0]==0)? 1:0;
+                Negative<=0;
+            
+            end 
+            4'b0011: begin //SLT
+                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+                
+                Zero<=(In_A- In_B==0)? 1:0;
+                Negative<=In_A - In_B;
+            
+            end 
+            4'b0100: begin //SLTU
+                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+                
+                Zero<=(In_A- In_B==0)? 1:0;
+                Negative<=In_A - In_B;
+            
+            end 
+            4'b0101: 4'b0100: begin //SLTU
+                ALU_out <= (In_A < In_B)? 32'd1:32'd0 ;
+                
+                Zero<=(In_A- In_B==0)? 1:0;
+                Negative<=In_A - In_B;
+            
+            end 
+            4'b0110: ALU_out <= In_A - In_B;
+            4'b0111: ALU_out <= 0;  //SLT
+            4'b1000:
+            4'b1001:
+            default : ALU_out= In_A;
+        endcase 
     
     end
     
